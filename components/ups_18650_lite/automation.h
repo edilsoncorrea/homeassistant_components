4804d3dfe1e3f8d945a53b30d11a376fd36adb78
edilsoncorrea <edilsoncorrea117@gmail.com>
2025-03-03 10:46:46 -0300

    Primeiro teste com o componente ups_18650_lite

---
+++ components/ups_18650_lite/automation.h
@@ -0,0 +1,20 @@
+
+#pragma once
+#include "esphome/core/automation.h"
+#include "ups_18650_lite.h"
+
+namespace esphome {
+namespace ups_18650_lite {
+
+template<typename... Ts> class SleepAction : public Action<Ts...> {
+ public:
+  explicit SleepAction(ups_18650_liteComponent *ups_18650_lite) : ups_18650_lite_(ups_18650_lite) {}
+
+  void play(Ts... x) override { this->ups_18650_lite_->sleep_mode(); }
+
+ protected:
+  UPS_18650_LITEComponent *ups_18650_lite_;
+};
+
+}  // namespace ups_18650_lite
+}  // namespace esphome
