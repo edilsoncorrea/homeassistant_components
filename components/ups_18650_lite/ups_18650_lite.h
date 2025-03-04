954b7e13046269bf7f7dd2d03f6cf47144b42398
edilsoncorrea <edilsoncorrea117@gmail.com>
2025-03-04 12:03:52 -0300

    Adição da  #include "Arduino.h"

--- components/ups_18650_lite/ups_18650_lite.h
+++ components/ups_18650_lite/ups_18650_lite.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "Arduino.h"
+
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
